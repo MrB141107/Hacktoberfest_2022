4e9ba6c6546ede1a4e3fff6496a4ab739b1793b1
Sabyasachi Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-10-03 17:37:02 +0530

    Added TicTacToe

---
+++ Python/TicTacToe.py
@@ -0,0 +1,123 @@
+import random
+import tkinter
+from tkinter import *
+from functools import partial
+from tkinter import messagebox
+from copy import deepcopy
+
+
+sign = 0
+
+global board
+board = [[" " for x in range(3)] for y in range(3)]
+def winner(b, l):
+    return ((b[0][0] == l and b[0][1] == l and b[0][2] == l) or
+            (b[1][0] == l and b[1][1] == l and b[1][2] == l) or
+            (b[2][0] == l and b[2][1] == l and b[2][2] == l) or
+            (b[0][0] == l and b[1][0] == l and b[2][0] == l) or
+            (b[0][1] == l and b[1][1] == l and b[2][1] == l) or
+            (b[0][2] == l and b[1][2] == l and b[2][2] == l) or
+            (b[0][0] == l and b[1][1] == l and b[2][2] == l) or
+            (b[0][2] == l and b[1][1] == l and b[2][0] == l))
+
+
+def get_text_pc(i, j, gb):
+    global sign
+    if board[i][j] == ' ':
+        if sign % 2 == 0:
+            board[i][j] = "X"
+        else:
+            button[i][j].config(state=ACTIVE)
+            board[i][j] = "O"
+        sign += 1
+        button[i][j].config(text=board[i][j])
+    x = True
+    if winner(board, "X"):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Winner", "Player won the match")
+    elif winner(board, "O"):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Winner", "Computer won the match")
+    elif(isfull()):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Tie Game", "Tie Game")
+    if(x):
+        if sign % 2 != 0:
+            move = pc()
+            button[move[0]][move[1]].config(state=DISABLED)
+            get_text_pc(move[0], move[1], gb)
+
+def pc():
+    possiblemove = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == ' ':
+                possiblemove.append([i, j])
+    move = []
+    if possiblemove == []:
+        return
+    else:
+        for let in ['O', 'X']:
+            for i in possiblemove:
+                boardcopy = deepcopy(board)
+                boardcopy[i[0]][i[1]] = let
+                if winner(boardcopy, let):
+                    return i
+        corner = []
+        for i in possiblemove:
+            if i in [[0, 0], [0, 2], [2, 0], [2, 2]]:
+                corner.append(i)
+        if len(corner) > 0:
+            move = random.randint(0, len(corner)-1)
+            return corner[move]
+        edge = []
+        for i in possiblemove:
+            if i in [[0, 1], [1, 0], [1, 2], [2, 1]]:
+                edge.append(i)
+        if len(edge) > 0:
+            move = random.randint(0, len(edge)-1)
+            return edge[move]
+
+# Check if the player can push the button or not
+def isfree(i, j):
+    return board[i][j] == " "
+
+# Check the board is full or not
+def isfull():
+    flag = True
+    for i in board:
+        if(i.count(' ') > 0):
+            flag = False
+    return flag
+
+
+def gameboard_pc(game_board):
+    global button
+    button = []
+    for i in range(3):
+        m = i
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text_pc, i, j, game_board)
+            button[i][j] = Button(
+                game_board, bd=5, command=get_t, height=4, width=8)
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
+
+def withpc():
+    game_board = Tk()
+    game_board.title("Unbeatable TicTacToe")
+    gameboard_pc(game_board)
+
+def play():
+    withpc()
+
+# Call main function
+if __name__ == '__main__':
+    play()
