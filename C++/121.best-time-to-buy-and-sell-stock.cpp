7924a55e1d35f73599dcd804d5f98787469d86de
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2022-10-05 00:06:55 +0530

    Fixes #15

--- C++/121.best-time-to-buy-and-sell-stock.cpp
+++ C++/121.best-time-to-buy-and-sell-stock.cpp
@@ -10,19 +10,6 @@ public:
     int maxProfit(vector<int>& prices) {
         int minPrice=prices[0];
         int profit=0;
-        //BruteForce:
-        // for(int i=1;i<prices.size();i++){
-        //     if(prices[i]>minPrice){
-        //         if(profit<(prices[i]-minPrice)){
-        //             profit=prices[i]-minPrice;
-        //         }
-        //     }
-        //     else if(minPrice>prices[i]){
-        //         minPrice=prices[i];
-        //     }
-        // }
-        // return profit;
-        // Optimised::
         for(int i=1;i<prices.size();i++){
             minPrice=min(minPrice, prices[i]);
             profit=max(profit, prices[i]-minPrice);
